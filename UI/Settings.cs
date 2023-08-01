1a176c171a2aa4579a41a796cd2dcc5f805aa3b0
DJDavid98 <djdavid98@protonmail.com>
2023-08-02 00:17:09 +0200

    Add ALL the customization option

---
+++ UI/Settings.cs
@@ -0,0 +1,22 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+
+namespace bsrpc.UI
+{
+    internal class Settings : PersistentSingleton<Settings>
+    {
+        [UIValue("multiplayer-detection")]
+        public bool multiplayerDetection { get { return PluginConfig.Instance.MultiplayerDetection; } set { PluginConfig.Instance.MultiplayerDetection = value; } }
+
+        [UIValue("multiplayer-party-info")]
+        public bool multiplayerPartyInfo { get { return PluginConfig.Instance.MultiplayerPartyInfo; } set { PluginConfig.Instance.MultiplayerPartyInfo = value; } }
+
+        [UIValue("large-image-cover")]
+        public bool largeImageCover { get { return PluginConfig.Instance.LargeImageSongCover; } set { PluginConfig.Instance.LargeImageSongCover = value; } }
+
+        [UIValue("show-elapsed-times")]
+        public bool showElapsedTimes { get { return PluginConfig.Instance.ShowElapsedTimes; } set { PluginConfig.Instance.ShowElapsedTimes = value; } }
+
+        [UIValue("small-image-map-type")]
+        public bool smallImageMapType { get { return PluginConfig.Instance.ShowMapType; } set { PluginConfig.Instance.ShowMapType = value; } }
+    }
+}
